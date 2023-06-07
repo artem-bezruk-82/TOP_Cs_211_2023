409c67dc5f086185a7d25a6d0eba50ba95edf555
Artem Bezruk <artem.bezruk@gmail.com>
2023-06-07 22:26:09 +0300

    Module#1 home work task#5 done

---
+++ Module_01_Introduction/homeWork/hw_01_task5/Program.cs
@@ -0,0 +1,32 @@
+﻿// КУРС: ПЛАТФОРМА MICROSOFT .NET И ЯЗЫК ПРОГРАММИРОВАНИЯ C#
+// Модуль 1
+// Тема: Введение в платформу Microsoft .NET. Основы языка программирования C#
+// ДОМАШНЕЕ ЗАДАНИЕ
+// Задание 5
+// Пользователь вводит с клавиатуры дату.
+// Приложение должно отобразить название сезона и дня недели. 
+// Например, если введено 22.12.2021, приложение должно отобразить Winter Wednesday.
+
+namespace hw_01_task5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter date, using DD.MM.YYYY format");
+
+            DateOnly date = DateOnly.Parse(Console.ReadLine() ?? string.Empty);
+
+            string season = date.Month switch
+            {
+                >= 3 and < 6 => "Spring",
+                >= 6 and < 9 => "Summer",
+                >= 9 and < 12 => "Autumn",
+                12 or (>= 1 and < 3) => "Winter",
+                _ => throw new ArgumentOutOfRangeException(nameof(date), $"Date with unexpected month: {date.Month}.")
+            };
+
+            Console.WriteLine($"{date} {date.DayOfWeek} {season}");
+        }
+    }
+}
