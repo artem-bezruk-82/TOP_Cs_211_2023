70e8aaea8284bb931cbf1ff17fa45fda7ba6d2ae
Artem Bezruk <artem.bezruk@gmail.com>
2023-06-07 22:50:18 +0300

    Module#1 home work task#7 done

---
+++ Module_01_Introduction/homeWork/cw_01_task7/Program.cs
@@ -0,0 +1,36 @@
+﻿// КУРС: ПЛАТФОРМА MICROSOFT .NET И ЯЗЫК ПРОГРАММИРОВАНИЯ C#
+// Модуль 1
+// Тема: Введение в платформу Microsoft .NET. Основы языка программирования C#
+// ДОМАШНЕЕ ЗАДАНИЕ
+// Задание 7
+// Пользователь вводит с клавиатуры два числа.
+// Нужно показать все четные числа в указанном диапазоне.
+// Если границы диапазона указаны неправильно требуется произвести нормализацию границ.
+// Например, пользователь ввел 20 и 11, требуется нормализация, после которой начало диапазона станет равно 11, а конец 20.
+
+namespace cw_01_task7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter start range value");
+            int rangeStart = int.Parse(Console.ReadLine() ?? String.Empty);
+            Console.WriteLine("Please enter end range value");
+            int rangeEnd = int.Parse(Console.ReadLine() ?? String.Empty);
+
+            if (rangeStart > rangeEnd)
+            {
+                (rangeStart, rangeEnd) = (rangeEnd, rangeStart);
+            }
+
+            for (int i = rangeStart; i <= rangeEnd; i++)
+            {
+                if ((i % 2 == 0) && (i != 0))
+                {
+                    Console.Write($" {i}");
+                }
+            }
+        }
+    }
+}
