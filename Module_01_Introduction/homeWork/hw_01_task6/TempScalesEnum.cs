6f54a8f0c73290b2115fb2fd3103d5073a8619ae
Artem Bezruk <artem.bezruk@gmail.com>
2023-06-07 22:45:58 +0300

    Module#1 home work task#6 done

---
+++ Module_01_Introduction/homeWork/hw_01_task6/TempScalesEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw_01_task6
+{
+    public enum TempScalesEnum
+    {
+        Celsius = 0
+        , Fahrenheit
+    }
+}
