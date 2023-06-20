ac3c24ed941a513b7198d230d04962ee00930753
Artem Bezruk <artem.bezruk@gmail.com>
2023-06-20 22:28:24 +0300

    Module#02 practice work task#2

---
+++ Module_02_String Array Enum/practiceWork/pw_02_task1/Program.cs
@@ -0,0 +1,51 @@
+﻿// КУРС: «ПЛАТФОРМА MICROSOFT .NET И ЯЗЫК ПРОГРАММИРОВАНИЯ C#
+// Тема: Массивы и строки
+// Модуль 2
+// Практическое задание
+
+// Задание 1
+// Создайте приложение, которое отображает количество чётных, нечётных, уникальных элементов массива.
+
+
+namespace pw_02_task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArr = { 4, 2, 0, 3, 1, 2, 0, 5, 1 };
+            Console.WriteLine(string.Join(' ', myArr));
+            Array.Sort(myArr);
+
+            int oddValuesCounter = 0;
+            int evenValuesCounter = 0;
+            int zeroesCounter = 0;
+            int? currentValue = null;
+
+            foreach (int item in myArr) 
+            {
+                if (item != currentValue) 
+                {
+                    if (item == 0) 
+                    {
+                        zeroesCounter++;
+                    } 
+                    else if (item % 2 == 0) 
+                        evenValuesCounter++;
+                    else if (item % 2 != 0) 
+                        oddValuesCounter++;
+
+
+                    currentValue = item;
+                }
+            }
+
+            Console.WriteLine($"Uniq elemets: {evenValuesCounter + oddValuesCounter + zeroesCounter}" +
+                $"\nThat are: \n\tUniq even values: {evenValuesCounter}; \n\tUniq odd values: {oddValuesCounter}; " +
+                $"\n\tZero: {zeroesCounter}; ");
+
+        }
+
+
+    }
+}
---
+++ Module_02_String Array Enum/practiceWork/pw_02_task2/Program.cs
@@ -0,0 +1,53 @@
+﻿// КУРС: «ПЛАТФОРМА MICROSOFT .NET И ЯЗЫК ПРОГРАММИРОВАНИЯ C#
+// Тема: Массивы и строки
+// Модуль 2
+// Практическое задание
+
+// Задание 2
+// Создайте приложение, отображающее количество значений в массиве меньше заданного параметра пользователем.
+// Например, количество значений меньших, чем 7 (7 введено пользователем с клавиатуры). 
+
+namespace pw_02_task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = { 1, 2, 4, 5, 3, 3, 3 };
+            int value = 3;
+            Console.WriteLine($"There are {LessThanValue(myArray, value)} values less than {value}");
+
+
+        }
+
+
+        static int LessThanValue(int[] array, int value)
+        {
+            Array.Sort(array);
+            if (value > array[array.GetUpperBound(0)])
+            {
+                return array.Length;
+            }
+            else if (value > array[array.GetLowerBound(0)])
+            {
+                while (value != array.Min())
+                {
+                    int index = Array.BinarySearch(array, value);
+                    if (index < 0)
+                    {
+                        value--;
+                    }
+                    else
+                    {
+                        while (array[index] == array[index - 1])
+                        {
+                            index--;
+                        }
+                        return index;
+                    }
+                }
+            }
+            return 0;
+        }
+    }
+}
