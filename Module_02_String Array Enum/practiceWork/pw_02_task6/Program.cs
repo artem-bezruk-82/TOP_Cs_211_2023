a701da108fc364eb26d81aa781be77e5e2be1db5
Artem Bezruk <artem.bezruk@gmail.com>
2023-06-21 08:02:23 +0300

    Module#02 practice work task#6

---
+++ Module_02_String Array Enum/practiceWork/pw_02_task6/Program.cs
@@ -0,0 +1,24 @@
+﻿// КУРС: «ПЛАТФОРМА MICROSOFT .NET И ЯЗЫК ПРОГРАММИРОВАНИЯ C#
+// Тема: Массивы и строки
+// Модуль 2
+// Практическое задание
+
+// Задание 6
+// Пользователь вводит предложение с клавиатуры. Вам необходимо подсчитать количество слов в нём. 
+
+namespace pw_02_task6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputStr = "  ** Hello  , , ( IT )  World  ! ***  ";
+            Console.WriteLine(inputStr);
+            char[] trimChar = { ' ', '*', '/' };
+            char[] delimiters = { ' ', ',', '.', '?', '!', ':', ';', '*', '(', ')' };
+            string[] words = inputStr.Trim(trimChar).Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+            Console.WriteLine(string.Join(' ', words) + ".");
+            Console.WriteLine($"Words: {words.Length}");
+        }
+    }
+}
