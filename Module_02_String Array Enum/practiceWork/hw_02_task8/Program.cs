85ae7e1492f2a259ba082a50c049774ac121480e
Artem Bezruk <artem.bezruk@gmail.com>
2023-06-21 08:14:43 +0300

    Module#02 practice work task#8

---
+++ Module_02_String Array Enum/practiceWork/hw_02_task8/Program.cs
@@ -0,0 +1,31 @@
+﻿// КУРС: «ПЛАТФОРМА MICROSOFT .NET И ЯЗЫК ПРОГРАММИРОВАНИЯ C#
+// Тема: Массивы и строки
+// Модуль 2
+// Практическое задание
+
+// Задание 8
+// Пользователь вводит с клавиатуры предложение.
+// Приложение должно посчитать количество гласных букв в нём
+
+namespace hw_02_task8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u', 'y' };
+            string userInput = "Hello, World!";
+            int numOfVowels = 0;
+
+            for (int i = 0; i < userInput.Length; i++)
+            {
+                if (Array.Exists(vowels, element => element == userInput[i]))
+                {
+                    numOfVowels++;
+                }
+            }
+
+            Console.WriteLine($"{userInput}\nVowels: {numOfVowels}");
+        }
+    }
+}
