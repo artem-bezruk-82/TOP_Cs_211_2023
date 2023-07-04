587393a2b519ef0360281e27e1e9e30ecbfe8f00
Artem Bezruk <artem.bezruk@gmail.com>
2023-07-04 21:08:56 +0300

    Module#03 material task#1

---
+++ Module_03_Class and Struct/material/material_03_task1/Article.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace material_03_task1
+{
+    public struct Article
+    {
+        public int Code { get; }
+        public string Name { get; set; }
+        public double? Price { get; set; }
+        public static int Count { get; private set; }
+
+        static Article() { Count = 0; }
+
+        public Article(string name, double? price)
+        {
+            Code = ++Count;
+            Name = name;
+            Price = price;
+        }
+
+        public Article(string name) : this(name, null) { }
+        public override string ToString()
+        {
+            return $"code: {Code}\tname: {Name}\tprice: {Price}";
+        }
+    }
+}
---
+++ Module_03_Class and Struct/material/material_03_task1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Курс: Платформа Microsoft .NET и язык программирования C#
+// Урок #3: Структуры. Классы. Свойства
+// Домашнее задание
+
+// Задание #1.
+// Описать структуру Article, содержащую следующие поля: код товара; название товара; цену товара.
+
+namespace material_03_task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Article articleA = new Article("article A", 2.5);
+            Console.WriteLine(articleA);
+            Article articleB = new Article("article B", 0.5);
+            Console.WriteLine(articleB);
+        }
+    }
+}
