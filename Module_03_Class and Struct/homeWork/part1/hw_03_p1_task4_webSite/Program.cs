0d0adbbd20921dd44a33085df361a622f92c8018
Artem Bezruk <artem.bezruk@gmail.com>
2023-07-05 19:07:03 +0300

    Module#03 home work part#1

---
+++ Module_03_Class and Struct/homeWork/part1/hw_03_p1_task4_webSite/Program.cs
@@ -0,0 +1,35 @@
+﻿// Курс: Платформа Microsoft .NET и язык программирования C#
+// Урок #3: Структуры. Классы. Свойства
+// Домашнее задание
+// Часть #1
+
+// Задание 4
+// Создайте класс «Веб-сайт».
+// Необходимо хранить в полях класса: название сайта, путь к сайту, описание сайта, ip адрес сайта.
+// Реализуйте методы класса для ввода данных, вывода данных, реализуйте доступ к отдельным полям через методы класса. 
+
+namespace hw_03_p1_task4_webSite
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WebSite google = new WebSite()
+            {
+                Name = "Google",
+                Uri = new Uri("https://google.com/"),
+                Description = "Google search service"
+            };
+
+            WebSite metanit = new WebSite()
+            {
+                Name = "Metenit",
+                Uri = new Uri("https://metanit.com/"),
+                Description = "Site about programming"
+            };
+
+            Console.WriteLine(google);
+            Console.WriteLine(metanit);
+        }
+    }
+}
