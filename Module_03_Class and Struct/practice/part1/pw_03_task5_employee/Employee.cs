6c748c8d081c30ffc7248f6e46763630365f6036
Artem Bezruk <artem.bezruk@gmail.com>
2023-07-14 17:00:39 +0300

    Module#03 practice work part#1 task#5

---
+++ Module_03_Class and Struct/practice/part1/pw_03_task5_employee/Employee.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pw_03_task5_employee
+{
+    public class Employee
+    {
+        public string? FirstName { get; set; }
+        public string? MiddleName { get; set; }
+        public string? LastName { get; set; }
+
+        public DateOnly? BirthDate { get; set; }
+        public string? Position { get; set; }
+        public string? Description { get; set; }
+        public MailAddress? Email { get; set; }
+
+        private string? _phone;
+        public string? Phone
+        {
+            get { return _phone; }
+            set
+            {
+                if (value?.Length != 11)
+                {
+                    throw new ArgumentOutOfRangeException(value, message: "Telephon number must contain 11 digits");
+                }
+
+                Int64 number;
+
+                if (Int64.TryParse(value, out number))
+                {
+                    _phone = $"+{value}";
+                }
+                else
+                {
+                    throw new ArgumentException("It is not a number");
+                }
+            }
+        }
+
+        public Employee(string? firtsName, string? middleName, string? lastName, DateOnly? birthDate)
+        {
+            FirstName = firtsName;
+            MiddleName = middleName;
+            LastName = lastName;
+            BirthDate = birthDate;
+        }
+
+        public Employee() : this(null, null, null, null) { }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {MiddleName} {LastName}, {BirthDate?.ToShortDateString()}, {Position}, " +
+                $"e-mail: {Email}, phone {Phone}";
+        }
+
+    }
+}
---
+++ Module_03_Class and Struct/practice/part1/pw_03_task5_employee/EmployeeExt.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pw_03_task5_employee
+{
+    public static class EmployeeExt
+    {
+        public static void SetItemConsole(this Employee employee)
+        {
+            Dictionary<int, string> menu = new Dictionary<int, string>() 
+            {
+                { 0, "APPLY settings"},
+                { 1, "Set First Name"},
+                { 2, "Set Middle Name"},
+                { 3, "Set Last Name"},
+                { 4, "Set Birthday date"},
+                { 5, "Set e-mail address"},
+                { 6, "Set phone number"},
+                { 7, "Set position title"},
+                { 8, "Set job description"},
+            };
+
+            int menuItem = -1;
+
+            while (menuItem != 0)
+            {   
+                Console.WriteLine("Employee properties menu");
+
+                foreach (KeyValuePair<int, string> item in menu)
+                {
+                    Console.WriteLine($"\t{item.Key} - {item.Value}");
+                }
+
+                menuItem = (int)GetConsoleInputInt("Please chose laptop parameter", 0, menu.Count);
+
+                switch (menuItem)
+                {
+                    case 1:
+                        SetFirstNameConsole(employee);
+                        break;
+                    case 2:
+                        SetMiddleNameConsole(employee);
+                        break;
+                    case 3:
+                        SetLastNameConsole(employee);
+                        break;
+                    case 4:
+                        SetBirthDateConsole(employee);
+                        break;
+                    case 5:
+                        SetEmailConsole(employee);
+                        break;
+                    case 6:
+                        SetPhoneConsole(employee);
+                        break;
+                    case 7:
+                        SetPositionConsole(employee);
+                        break;
+                    case 8:
+                        SetDescriptionConsole(employee);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        public static void SetFirstNameConsole(this Employee employee)
+        {
+            Console.WriteLine($"Please enter {nameof(employee.FirstName)}");
+            employee.FirstName = Console.ReadLine() ?? string.Empty;
+        }
+
+        public static void SetMiddleNameConsole(this Employee employee)
+        {
+            Console.WriteLine($"Please enter {nameof(employee.MiddleName)}");
+            employee.MiddleName = Console.ReadLine() ?? string.Empty;
+        }
+
+        public static void SetLastNameConsole(this Employee employee)
+        {
+            Console.WriteLine($"Please enter {nameof(employee.LastName)}");
+            employee.LastName = Console.ReadLine() ?? string.Empty;
+        }
+
+        public static void SetBirthDateConsole(this Employee employee) 
+        {
+            Console.WriteLine("Please enter established date, using DD.MM.YYYY format");
+            employee.BirthDate = DateOnly.Parse(Console.ReadLine() ?? string.Empty);
+        }
+
+        public static void SetEmailConsole(this Employee employee) 
+        {
+            Console.WriteLine("Please enter e-mail address");
+            employee.Email = new System.Net.Mail.MailAddress(Console.ReadLine() ?? string.Empty);
+        }
+
+        public static void SetPhoneConsole(this Employee employee) 
+        {
+            employee.Phone = $"{GetConsoleInputInt("Please enter 11-digits phone", 10000000000, 99999999999)}";
+        }
+
+        public static void SetPositionConsole(this Employee employee) 
+        {
+            Console.WriteLine("Please enter position title");
+            employee.Position = Console.ReadLine() ?? string.Empty;
+        }
+
+        public static void SetDescriptionConsole(this Employee employee)
+        {
+            Console.WriteLine("Please enter job description");
+            employee.Description = Console.ReadLine() ?? string.Empty;
+        }
+
+        private static Int64 GetConsoleInputInt(string requestText, 
+            Int64 startRange= Int64.MinValue, Int64 endRange = Int64.MaxValue)
+        {
+            Int64? value = null;
+
+            do 
+            {
+                while (value is null)
+                {
+                    Console.WriteLine(requestText);
+                    try
+                    {
+                        value = Int64.Parse(Console.ReadLine() ?? "");
+                    }
+                    catch (Exception exc)
+                    {
+
+                        Console.WriteLine($"{exc.Message} Please enter integer value");
+                    }
+                }
+                if (value < startRange || value > endRange)
+                    throw new ArgumentOutOfRangeException(nameof(value), value , 
+                        message: $"value is out of {startRange}...{endRange} value");
+            } while (value < startRange || value > endRange);
+
+            return (Int64)value;
+        }
+    }
+}
